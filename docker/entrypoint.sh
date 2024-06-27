2031626ade5c49ecd40d9aa81a0ebc96f9b43cbc
Andreas Kloeckner <inform@tiker.net>
2024-06-27 13:16:37 -0500

    ci-base-image: entrypoint hackery to make sure PATH gets set

---
+++ docker/entrypoint.sh
@@ -0,0 +1,15 @@
+# According to
+# https://docs.gitlab.com/runner/shells/#shell-profile-loading
+# Gitlab runner's docker executor does not load shell startup files,
+# because that's a great idea.
+cmd=()
+for i in "$@"; do
+  cmd+=("$(echo "$i"| sed "/exec/s/bash/bash --login/g")")
+done
+echo "Using rewritten shell invocation received from executor."
+echo "${cmd[@]}"
+"${cmd[@]}"
+retcode="$?"
+echo "return code from command: $retcode"
+exit "$retcode"
+
